3ee67feee6c23c51191e2033907fad9eae774e17
Masood404 <yaquobimasood@gmail.com>
2023-11-23 15:02:36 +0100

    Added the Users class to manipulate user data from the databse

---
+++ includes/Users.php
@@ -0,0 +1,80 @@
+<?php
+    require_once __DIR__ . DIRECTORY_SEPARATOR . "DBConn.php";
+    class Users{
+        private static string $privateKey = MY_CONFIG["Private_Key"];
+        private static string $publicKey = MY_CONFIG["Public_Key"];
+
+        private function __construct(){
+            //Uninstantiable 
+        }
+        public static function createUser(string $username, string $password, ?string $email = null, ?string $subscription = null){
+            global $_DBConn;
+
+            $encPass = self::encrypt($password);
+            $encEmail = $email != null ? self::encrypt($email) : null;
+
+            $query = "INSERT INTO users (username, encPass, encEmail, subscription)
+            VALUES (?, ?, ?, ?);";
+
+            $stmt = $_DBConn->prepare($query);
+            if(!$stmt){
+                die("_DBConn Preperation failed: " . $_DBConn->error);
+            }
+
+            $stmt->bind_param("ssss", $username, $encPass, $encEmail, $subscription);
+            $stmt->execute();
+        }
+        public static function getUser(string $username){
+            global $_DBConn; 
+
+            $query = "SELECT * FROM users WHERE username = ?;";
+
+            $stmt = $_DBConn->prepare($query);
+            if(!$stmt){
+                die("_DBConn Preperation failed: " . $_DBConn->error);
+            }
+
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            
+            $enc_user = $stmt->get_result()->fetch_assoc();
+            $user = [
+                "username" => $enc_user["username"],
+                "password" => self::decrypt($enc_user["encPass"]),
+                "email" => self::decrypt($enc_user["encEmail"]),
+                "subscription" => self::decrypt($enc_user["subscription"])
+            ];
+
+            return $user;
+        }
+        private static function encrypt(string $data){
+            $pubKey = openssl_pkey_get_public(self::$publicKey);
+
+            openssl_public_encrypt($data, $enc_data, self::$publicKey);
+            $enc_data = base64_encode($enc_data);
+            return $enc_data;
+        }
+        public static function decrypt(string $enc_data){
+            $enc_data = base64_decode($enc_data);
+            $privKey = openssl_pkey_get_private(self::$privateKey);
+
+            openssl_private_decrypt($enc_data, $dec_data, $privKey);
+
+            return $dec_data;
+        }
+    }
+
+    try{
+        Users::createUser("masood", "masood123");
+    }
+    catch(mysqli_sql_exception $e){
+        if($e->getCode() == 1062){
+            //Duplicate username entry
+            die("Duplicate entry for username");
+        }
+        else{
+            die("Error: " . $e->getMessage());
+        }
+    }  
+   
+?>
