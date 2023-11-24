cadc772bdc90ffda7efb2c01362f2dc62d6115a2
Masood404 <yaquobimasood@gmail.com>
2023-11-24 23:08:29 +0100

    Added the functionality for the parameter $subscription, where when the parameter type is either an array or Subscription, It will be converted to json to store in the database, if the type is string it will check if its json formatted.

--- includes/Users.php
+++ includes/Users.php
@@ -1,5 +1,9 @@
 <?php
     require_once __DIR__ . DIRECTORY_SEPARATOR . "DBConn.php";
+    require_once __VENDOR__ . DIR_S . "autoload.php";
+
+    use Minishlink\WebPush\Subscription;
+
     class Users{
         private static string $privateKey = MY_CONFIG["Private_Key"];
         private static string $publicKey = MY_CONFIG["Public_Key"];
@@ -7,12 +11,33 @@
         private function __construct(){
             //Uninstantiable 
         }
-        public static function createUser(string $username, string $password, ?string $email = null, ?string $subscription = null){
+        /**
+         * @param string|array|null $subscription this string should be json formatted
+         */
+        public static function createUser(string $username, string $password, ?string $email = null, string|array|Subscription|null $subscription = null){
             global $_DBConn;
 
             //Re-encrypt user password and email to add to the database.
             $encPass = self::encrypt($password);
             $encEmail = $email != null ? self::encrypt($email) : null;
+
+            //Handle subscription types by converting any other type to json formatted string so it can be stored in the db.
+            if($subscription instanceof Subscription || is_array($subscription)){
+                $json_encoded = json_encode($subscription, JSON_PRETTY_PRINT);
+                if($json_encoded === false){
+                    die("Users: failed encoding the subscription to json");
+                }
+                else{
+                    $subscription = $json_encoded;
+                }
+            }
+            elseif(is_string($subscription)){
+                //Check if the string is in json format
+                $decoded = json_decode($subscription);
+                if($decoded == null || $subscription !== "null"){
+                    die("Users: subscription is not formatted in json");
+                }
+            }
 
             $query = "INSERT INTO users (username, encPass, encEmail, subscription)
             VALUES (?, ?, ?, ?);";
@@ -25,7 +50,11 @@
             $stmt->bind_param("ssss", $username, $encPass, $encEmail, $subscription);
             $stmt->execute();
         }
-        public static function getUser(string $username){
+        /**
+         * Get the user data 
+         * @return array|null
+         */
+        public static function getUser(string $username) : array|null{
             global $_DBConn; 
 
             $query = "SELECT * FROM users WHERE username = ?;";
