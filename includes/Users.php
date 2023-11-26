b1b21cd3a44e493327de622e76c5f696f166ad76
Masood404 <yaquobimasood@gmail.com>
2023-11-26 15:53:00 +0100

    Removed getting password with the getUser() method, Instead of encrypting password to the database. The passwords are now hashed in the database. Created a new method exists(). To get hashed password from the database, created a new method getHashPass(). And lastly created another method which authenticates users and returns with a jwt token on success.

--- includes/Users.php
+++ includes/Users.php
@@ -2,11 +2,14 @@
     require_once __DIR__ . DIRECTORY_SEPARATOR . "DBConn.php";
     require_once __VENDOR__ . DIR_S . "autoload.php";
 
-    use Minishlink\WebPush\Subscription;
+use Firebase\JWT\Key;
+use Minishlink\WebPush\Subscription;
+use Firebase\JWT\JWT;
 
     class Users{
-        private static string $privateKey = MY_CONFIG["Private_Key"];
-        private static string $publicKey = MY_CONFIG["Public_Key"];
+        private static string $_privateKey = MY_CONFIG["Private_Key"];
+        private static string $_publicKey = MY_CONFIG["Public_Key"];
+        private static string $_jwtAlgo = "RS256";
 
         private function __construct(){
             //Uninstantiable 
@@ -17,8 +20,8 @@
         public static function createUser(string $username, string $password, ?string $email = null, string|array|Subscription|null $subscription = null){
             global $_DBConn;
 
-            //Re-encrypt user password and email to add to the database.
-            $encPass = self::encrypt($password);
+            //Hash the password and rencrypt the email.
+            $hashPass = password_hash($password, PASSWORD_DEFAULT);
             $encEmail = $email != null ? self::encrypt($email) : null;
 
             //Handle subscription types by converting any other type to json formatted string so it can be stored in the db.
@@ -39,7 +42,7 @@
                 }
             }
 
-            $query = "INSERT INTO users (username, encPass, encEmail, subscription)
+            $query = "INSERT INTO users (username, hashPass, encEmail, subscription)
             VALUES (?, ?, ?, ?);";
 
             $stmt = $_DBConn->prepare($query);
@@ -47,11 +50,11 @@
                 die("_DBConn Preperation failed: " . $_DBConn->error);
             }
 
-            $stmt->bind_param("ssss", $username, $encPass, $encEmail, $subscription);
+            $stmt->bind_param("ssss", $username, $hashPass, $encEmail, $subscription);
             $stmt->execute();
         }
         /**
-         * Get the user data 
+         * Get the user data such as username, user email, user subscription.
          * @return array|null
          */
         public static function getUser(string $username) : array|null{
@@ -61,37 +64,121 @@
 
             $stmt = $_DBConn->prepare($query);
             if(!$stmt){
-                die("_DBConn Preperation failed: " . $_DBConn->error);
+                throw new Exception("Preperation failed: " . $_DBConn->error);
             }
 
             $stmt->bind_param("s", $username);
             $stmt->execute();
+
+            $result = $stmt->get_result();
+            if($result->num_rows === 0){
+                //username not found
+                return null;
+            }
             
-            $enc_user = $stmt->get_result()->fetch_assoc();
+            $enc_user = $result->fetch_assoc();
             $user = [
                 "username" => $enc_user["username"],
-                "password" => self::decrypt($enc_user["encPass"]),
                 "email" => isset($enc_user["encEmail"]) ? self::decrypt($enc_user["encEmail"]) : null,
                 "subscription" => isset($enc_user["subscription"]) ? self::decrypt($enc_user["subscription"]) : null
             ];
 
             return $user;
         }
-        private static function encrypt(string $data){
-            $pubKey = openssl_pkey_get_public(self::$publicKey);
+        /**
+         * Checks if the user exists
+         */
+        public static function exists(string $username) : bool{
+            global $_DBConn;
 
-            openssl_public_encrypt($data, $enc_data, self::$publicKey);
+            $query = "SELECT CASE 
+            WHEN EXISTS (SELECT 1 FROM users WHERE username = ?) 
+            THEN 1 
+            ELSE 0 
+            END AS username_exists;";
+
+            $stmt = $_DBConn->prepare($query);
+
+            if(!$stmt){
+                throw new Exception("_DBConn Preperation failed: " . $_DBConn->error);
+            }
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+
+            $exists = $stmt->get_result()->fetch_assoc()["username_exists"];
+
+            return $exists;
+        }
+        /**
+         * @return string An Authorization token
+         */
+        public static function Authenticate(string $username, string $password, null|int|DateTime $expiration = null) : string{
+            if(!Users::exists($username)){
+                throw new Exception("Authentication failed: Invalid username");
+            }
+
+            $hashPass = Users::getHashPass($username);
+
+            if(!password_verify($password, $hashPass)){
+                throw new Exception("Authentication failed: Invalid password");
+            }
+            if($expiration instanceof DateTime){
+                $expiration = $expiration->getTimestamp();
+            }
+            elseif(is_null($expiration)){
+                $expiration = time() + 30 * 30; //After 30 minutes
+            }
+
+            $tokenPayload = [
+                "usr" => $username,
+                "exp" => $expiration
+            ];
+
+            $token = JWT::encode($tokenPayload, self::$_privateKey, self::$_jwtAlgo);
+
+            return $token;
+
+        }
+        /**
+         * @return null|string
+         */
+        public static function getHashPass(string $username) : string|null{
+            global $_DBConn;
+
+            $query = "SELECT hashPass FROM users WHERE username = ?";
+            $stmt = $_DBConn->prepare($query);
+
+            if(!$stmt){
+                throw new Exception("Preperation failed: " . $_DBConn->error);
+            }
+
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+            if($result->num_rows === 0){
+                //username does not exist 
+                return null;
+            }
+            $hasPass = $result->fetch_assoc()["hashPass"];
+
+            return $hasPass;
+            
+        }
+        public static function encrypt(string $data){
+            $pubKey = openssl_pkey_get_public(self::$_publicKey);
+
+            openssl_public_encrypt($data, $enc_data, self::$_publicKey);
             $enc_data = base64_encode($enc_data);
             return $enc_data;
         }
         public static function decrypt(string $enc_data){
             $enc_data = base64_decode($enc_data);
-            $privKey = openssl_pkey_get_private(self::$privateKey);
+            $privKey = openssl_pkey_get_private(self::$_privateKey);
 
             openssl_private_decrypt($enc_data, $dec_data, $privKey);
 
             return $dec_data;
         }
     }
-   
 ?>
