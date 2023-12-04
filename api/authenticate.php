befcae20822a66e75a81f318ce8fb6776353c0a6
Masood404 <yaquobimasood@gmail.com>
2023-12-04 17:10:29 +0100

    created a test authentication endpoint with no decryption for now. It returns the authentication token which then will be stored in the local storage of the frontend. From frontend when making requests to other enpoints, the authorzation header is required which should contain the bearer token provided by this endpoint.

---
+++ api/authenticate.php
@@ -0,0 +1,23 @@
+<?php
+    /*
+        TEST FILE
+    */
+
+    require_once realpath(__DIR__."/../includes/api_init.php");
+    require_once __INCLUDES__.DIR_S."Users.php";
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, X-Requested-With");
+
+    if(!isset($_POST["username"]) && !isset($_POST["password"])){
+        sendErrorResponse("Invalid credentials", 401);
+    }
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    echo json_encode(["token" => Users::Authenticate($username, $password)]);
+
+?>
