6a9470f49831d5b7d1f8b86dc7554ad9007e527c
Masood404 <yaquobimasood@gmail.com>
2023-12-06 21:37:23 +0100

    Moved style.css inside a new directory css/static/. Instead of it created the style.php to handle it dynamically, also in the front controller the uri /style.css points towards style.php.

---
+++ assets/css/style.php
@@ -0,0 +1,12 @@
+<?php 
+    require_once realpath(__DIR__."/../../includes/constants.php");
+
+    header("Content-type: text/css; chatset: UTF-8");
+
+    $dynamicStyle = "@import url('".path_to_url(__NODE_MODULES__)."/@flaticon/flaticon-uicons/css/all/all.css');\n";
+    $staticStyle = file_get_contents(realpath(__DIR__."/static/style.css"));
+
+    echo $dynamicStyle.$staticStyle;
+?>
+
+
