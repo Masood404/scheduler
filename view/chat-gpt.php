8b84def3fa6087aff73e5bea91a0f0b867a6849a
Masood404 <yaquobimasood@gmail.com>
2023-12-10 23:30:36 +0100

    Created the new element chat selection, So far its functionality is to select and delete chat(s). In the css changed the loader's styles.

--- view/chat-gpt.php
+++ view/chat-gpt.php
@@ -25,26 +25,31 @@
                     <span>New Chat</span>
                 </button>
             </div>
+            <div id="chat_selection_wrapper">
+                <label for="chat_selection_box" id="chat_selection">
+                    <input type="checkbox" id="chat_selection_box">
+                    Select
+                </label>
+                <i id="chat_selection_delete" class="fi fi-rr-trash"></i>
+            </div>
             <div class="chatHistory">
-                <div class="h_chatBlock">
-                    <i class="fi fi-rr-messages h_chatIcon"></i>
-                    <div class="h_chatTitle">Chat Title</div>
-                </div>
+                <!-- Inject through js -->
             </div>
         </div>
         <div class="chatContainer">
             <div class="chatMessages">
                 <div class="defaultMessageContainer">
-                    <img src="/scheduler/assets/images/transparent gpt icon.png">
+                    <img src="<?php echo path_to_url(__IMAGES__); ?>/transparent gpt icon.png">
                     <h2>Chat Gpt</h2>
                     <p>
-                        Chatbot GPT, or Generative Pre-trained Transformer, is a sophisticated AI model developed by OpenAI.
-                        It is designed to assist users with various tasks and provide helpful responses in conversational style.
-                        GPT utilizes transformers, a type of neural network architecture, to understand and generate human-like
-                        text based on the input received. With its vast pre-training on a wide range of data, GPT has the ability
-                        to generate coherent and contextually relevant answers to users' queries on any subject.
+                    Chatbot GPT, or Generative Pre-trained Transformer, is a sophisticated AI model developed by OpenAI.
+                    It is designed to assist users with various tasks and provide helpful responses in conversational style.
+                    GPT utilizes transformers, a type of neural network architecture, to understand and generate human-like
+                    text based on the input received. With its vast pre-training on a wide range of data, GPT has the ability
+                    to generate coherent and contextually relevant answers to users' queries on any subject.
                     </p>
                 </div>
+                <!-- Inject through js -->
             </div>
             <div class="chatSendMessageWrapper">
                 <div class="chatSendMessageContainer">
